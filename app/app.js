b954f99d3ddd9a20b178abd1d604cccfffb12584
miguelapol <apolonio1995@gmail.com>
2022-04-28 14:32:31 -0500

    Viendo como funciona fizzbuzz

--- app/app.js
+++ app/app.js
@@ -60,5 +60,6 @@ const assignFizzBuzzTrick = function(explorer){
 };
 
 const explorersInNodeAndFizzBuzzTrick = explorersInNode.map((explorer) => assignFizzBuzzTrick(explorer));
+console.log(explorersInNodeAndFizzBuzzTrick);
 
 // Part 8: Get a list of the explorers in node, if the score is divisible by 5 and 3, set the property trick and the value FIZZBUZZ, if is just divisible by 5 set the property trcik and the value BUZZ, if is just divisible by 3 set the property trick and the value FIZZ, otherwise set the property trick and the score value. TODO
