7fdb6e5f081ad1edbe08e1764a9e330e591a3ed1
miguelapol <apolonio1995@gmail.com>
2022-04-28 15:27:09 -0500

    validaciones del explorer usando el fizzservice

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const Reader=require("./lib/utils/Reader");
 const ExplorerService=require("./lib/services/ExplorerService");
+const FizzbuzzService=require("./lib/services/FizzbuzzService");
 const explorers=Reader.readJSONFile("./app/explorers.json");
 console.log(ExplorerService.filterbyMission(explorers, "node"));
 console.log(ExplorerService.getExplorerUsernameByMission(explorers));
 console.log(ExplorerService.getAmountOfExplorerByMission(explorers, "node"));
+const explorer1 = {name: "Explorer1", score: 1}
+console.log(FizzbuzzService.applyValidationInExplorer(explorer1));
+const explorer2 = {name: "Explorer2", score: 3}
+console.log(FizzbuzzService.applyValidationInExplorer(explorer2));
+const explorer3 = {name: "Explorer3", score: 5}
+console.log(FizzbuzzService.applyValidationInExplorer(explorer3));
+const explorer4 = {name: "Explorer4", score: 15}
+console.log(FizzbuzzService.applyValidationInExplorer(explorer4));
