dd5480e7b9a426bb96b317b5f24ce2496fc66458
miguelapol <apolonio1995@gmail.com>
2022-04-28 08:45:48 -0500

    Archivos de trabajo

---
+++ index.js
@@ -0,0 +1,6 @@
+const Reader=require("./lib/utils/Reader");
+const ExplorerService=require('./lib/services/ExplorerService');
+const explorers=Reader.readJSONFile("./app/explorers.json");
+console.log(ExplorerService.filterbyMission(explorers, "node"));
+console.log(ExplorerService.getExplorerUsernameByMission(explorers));
+console.log(ExplorerService.getAmountOfExplorerByMission(explorers, "node"));
