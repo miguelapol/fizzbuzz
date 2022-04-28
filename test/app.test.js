81219c69f3aa55f3654469b1eea87686f2c6bb17
miguelapol <apolonio1995@gmail.com>
2022-04-28 08:46:24 -0500

    Pruebas de calidad de software

---
+++ test/app.test.js
@@ -0,0 +1,26 @@
+
+const ExplorerService=require('./../lib/services/ExplorerService');
+
+
+
+describe('Pruebas de unidad', function() {
+    test('Filters of explorers', function() {
+        const input=[{id:3,mission: "node"}];
+        const out=[{id:3,mission: "node"}];
+        const explorersInNode=ExplorerService.filterbyMission(input, "node");
+        expect(explorersInNode).toEqual(out);
+        
+    });
+    test('Amount by mission', function() {
+        const input=[{id:3,mission: "node"}];
+        expect(ExplorerService.getAmountOfExplorerByMission(input, "node")).toBe(1);        
+    });
+    test('User name', function() {
+        const input=[{githubUsername: "apol"}];
+        const out=["apol"];
+        const explorersInNode=ExplorerService.getExplorerUsernameByMission(input);
+        //hacer un expect para que me duelva un valor de un array
+        expect(explorersInNode).toEqual(out);
+        
+    });
+});
---
+++ test/reader.test.js
@@ -0,0 +1,11 @@
+const Reader=require("./../lib/utils/Reader");
+const explorers=Reader.readJSONFile("./app/explorers.json");
+
+
+describe('Pruebas de unidad', function() {
+    test('Read of explorers', function() {
+      
+        expect(explorers).not.toBeUndefined();
+        
+    });
+});
