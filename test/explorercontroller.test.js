14d01337410f148d61306ed6e4f5cb9d0a30125b
miguelapol <apolonio1995@gmail.com>
2022-04-29 16:05:53 -0500

    aqui ponems a prueba el uso de fizzbuzz test

--- test/explorercontroller.test.js
+++ test/explorercontroller.test.js
@@ -1,17 +1,17 @@
 const controller=require("./../lib/controllers/ExplorerController");
 
 describe("Pruebas de unidad", function() {
-    test("controllers explorers amount", function() {
-        expect(controller.getAmountOfExplorerByMission("node")).toBe(1);
-    });
-    test("controllers explorers filter mission", function() {
-        const out=[{githubUsername:"apol",score:1,mission:"node"}];
-        expect(controller.getExplorersByMission("node")).toEqual(out);
-    });
-    test("controllers explorers Usernameby Mission", function() {
-        const out=["apol"];
-        expect(controller.getExplorerUsernameByMission("node")).toEqual(out);
-    });
+    // test("controllers explorers amount", function() {
+    //     expect(controller.getAmountOfExplorerByMission("node")).toBe(1);
+    // });
+    // test("controllers explorers filter mission", function() {
+    //     const out=[{githubUsername:"apol",score:1,mission:"node"}];
+    //     expect(controller.getExplorersByMission("node")).toEqual(out);
+    // });
+    // test("controllers explorers Usernameby Mission", function() {
+    //     const out=["apol"];
+    //     expect(controller.getExplorerUsernameByMission("node")).toEqual(out);
+    // });
     test("controllers fizzbuzz", function() {
         const out="FIZZ";
         expect(controller.fizzbuzz(3)).toEqual(out);
