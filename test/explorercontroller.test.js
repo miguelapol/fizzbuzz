66e34ef11570ee07f2e6f8ab0260d5d86a375f30
miguelapol <apolonio1995@gmail.com>
2022-04-29 14:38:30 -0500

    prueba de funciona correctamente el controller

--- test/explorercontroller.test.js
+++ test/explorercontroller.test.js
@@ -2,6 +2,14 @@ const controller=require('./../lib/controllers/ExplorerController');
 
 describe("Pruebas de unidad", function() {
     test("controllers explorers amount", function() {
-           expect(controller.getAmountOfExplorerByMission('node')).toBe(10);
+           expect(controller.getAmountOfExplorerByMission('node')).toBe(1);
     });
+    test("controllers explorers filter mission", function() {
+        const out=[{githubUsername:"apol",score:1,mission:"node"}];
+        expect(controller.getExplorersByMission('node')).toEqual(out);
+     });
+     test("controllers explorers Usernameby Mission", function() {
+        const out=["ap "];
+        expect(controller.getExplorerUsernameByMission('node')).toEqual(out);
+     });
 });
